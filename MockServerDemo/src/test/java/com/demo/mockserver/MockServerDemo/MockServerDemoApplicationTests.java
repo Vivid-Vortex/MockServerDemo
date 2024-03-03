1a01affc37ec98f8892fdf9aad228457fa191691
Vivid-Vortex <ctntc9.dpk@gmail.com>
2024-03-03 17:31:37 +0530

    Add mock server
    Mock server added

--- MockServerDemo/src/test/java/com/demo/mockserver/MockServerDemo/MockServerDemoApplicationTests.java
+++ MockServerDemo/src/test/java/com/demo/mockserver/MockServerDemo/MockServerDemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.demo.mockserver.MockServerDemo;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockserver.integration.ClientAndServer;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 
 @SpringBootTest
 class MockServerDemoApplicationTests {
+	private static ClientAndServer mockServer;
+
+	@BeforeAll
+	public static void startServer() {
+		mockServer = startClientAndServer(1080);
+		System.out.println("MockServer Started :"+ mockServer);
+	}
 
 	@Test
 	void contextLoads() {
+	}
+
+	@AfterAll
+	public static void stopServer() {
+		mockServer.stop();
 	}
 
 }
