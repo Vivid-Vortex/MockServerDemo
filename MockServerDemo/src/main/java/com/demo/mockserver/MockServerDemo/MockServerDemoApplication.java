58d63ff42986ec1ec394526f8a686fed60f18e50
Vivid-Vortex <ctntc9.dpk@gmail.com>
2024-03-03 16:07:31 +0530

    initial commit

---
+++ MockServerDemo/src/main/java/com/demo/mockserver/MockServerDemo/MockServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mockserver.MockServerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockServerDemoApplication.class, args);
+	}
+
+}
